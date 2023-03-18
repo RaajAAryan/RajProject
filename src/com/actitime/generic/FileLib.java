42e65ceb53e89a659af9dd963d9332fdc171c810
USER <USER@DESKTOP-5JHJNDH>
2023-03-18 13:45:10 +0530

    Hello

--- src/com/actitime/generic/FileLib.java
+++ src/com/actitime/generic/FileLib.java
@@ -10,18 +10,19 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class FileLib {
 
-public String getPropertyData(String key) throws IOException {
-FileInputStream fis=new FileInputStream("./data/commondata.property");
-Properties p=new Properties();
-p.load(fis);
-String data = p.getProperty(key);
-return data;
+	public String getPropertyData(String key) throws IOException {
+		FileInputStream fis = new FileInputStream("./data/commondata.property");
+		Properties p = new Properties();
+		p.load(fis);
+		String data = p.getProperty(key);
+		return data;
 	}
-public String getExcelData(String sheetName,int row,int cell) throws EncryptedDocumentException, IOException {
-FileInputStream fis=new FileInputStream("./data/testscript.xlsx");
-Workbook wb=WorkbookFactory.create(fis);
-String data=wb.getSheet(sheetName).getRow(row).getCell(cell).getStringCellValue();
-return data;
-}
+
+	public String getExcelData(String sheetName, int row, int cell) throws EncryptedDocumentException, IOException {
+		FileInputStream fis = new FileInputStream("./data/testscript.xlsx");
+		Workbook wb = WorkbookFactory.create(fis);
+		String data = wb.getSheet(sheetName).getRow(row).getCell(cell).getStringCellValue();
+		return data;
+	}
 
 }
--- src/com/actitime/testscript/CustomerModule.java
+++ src/com/actitime/testscript/CustomerModule.java
@@ -16,27 +16,27 @@ import com.actitime.pom.HomePage;
 import com.actitime.pom.TaskListPage;
 
 @Listeners(com.actitime.generic.ListenerImplementation.class)
-public class CustomerModule extends BaseClass{
-@Test
-public void testCreateCustomer() throws InterruptedException, EncryptedDocumentException, IOException {
-Reporter.log("CreateCustomer",true);
-FileLib f=new FileLib();
-String customerName = f.getExcelData("CreateCustomer", 1, 2);
-String customerDescprition = f.getExcelData("CreateCustomer", 1, 3);
-HomePage h=new HomePage(driver);
-h.setTaskTab();
-TaskListPage t=new TaskListPage(driver);
-t.getAddNewBtn().click();
-t.getNewCustomerOption().click();
-t.getCustomerNameTbx().sendKeys(customerName);
-t.getCustomerDescriptionTbx().sendKeys(customerDescprition);
-t.getSelectCustomerDD().click();
-t.getOurCompanyTx().click();
-t.getCreateCustomerBtn().click();
-WebDriverWait wait=new WebDriverWait(driver, 10);
-wait.until(ExpectedConditions.textToBePresentInElement(t.getActualCustomer(), customerName));
-String actualText = t.getActualCustomer().getText();
-Assert.assertEquals(actualText, customerName);
-}
-	
+public class CustomerModule extends BaseClass {
+	@Test
+	public void testCreateCustomer() throws InterruptedException, EncryptedDocumentException, IOException {
+		Reporter.log("CreateCustomer", true);
+		FileLib f = new FileLib();
+		String customerName = f.getExcelData("CreateCustomer", 1, 2);
+		String customerDescprition = f.getExcelData("CreateCustomer", 1, 3);
+		HomePage h = new HomePage(driver);
+		h.setTaskTab();
+		TaskListPage t = new TaskListPage(driver);
+		t.getAddNewBtn().click();
+		t.getNewCustomerOption().click();
+		t.getCustomerNameTbx().sendKeys(customerName);
+		t.getCustomerDescriptionTbx().sendKeys(customerDescprition);
+		t.getSelectCustomerDD().click();
+		t.getOurCompanyTx().click();
+		t.getCreateCustomerBtn().click();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.textToBePresentInElement(t.getActualCustomer(), customerName));
+		String actualText = t.getActualCustomer().getText();
+		Assert.assertEquals(actualText, customerName);
+	}
+
 }
